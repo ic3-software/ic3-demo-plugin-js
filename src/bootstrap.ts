426dec239cc58933170996d0863b39a46fbebe34
marc-polizzi <marc.polizzi@gmail.com>
2023-01-02 07:56:50 +0100

    releasing v8.3.0

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,7 +1,17 @@
 import PluginDefinition from "./PluginDefinition";
+// import {DashboardsLoaderFrame} from '@ic3/reporting-api'
 
 const plugin = PluginDefinition();
 const info = "Exporting the plugin : " + plugin.id + " [v" + plugin.apiVersion.getInfo() + "]"
 
 const elem = document.getElementById("info");
 elem && (elem.innerHTML = info);
+
+// dev: check no dependencies on @mui/material
+// DashboardsLoaderFrame({
+//     containerId: "info",
+//     frameId: "ff",
+//     onReady: () => {
+//     },
+//     url: "http://nowhere.com/icCube/report/console"
+// });
